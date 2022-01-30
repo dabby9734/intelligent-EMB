dabby9734/intelligent-EMB

commit b6b91320c555161cfb9b7fbc189d6068c7ea4694

refactor: remove repeated usage of refreshToken()

src/components/PostContent.jsx
@@ -1,47 +1,21 @@
-import { getCookie, setCookie } from "../lib/cookieMonster";
+import { getCookie } from "../lib/cookieMonster";
 import download from "downloadjs";
 
 import Button from "@mui/material/Button";
 import DownloadIcon from "@mui/icons-material/Download";
 import { Grid } from "@mui/material";
+import { refreshToken } from "../lib/browserMonster";
 
 const PostContent = ({ attachments, setInfo }) => {
-  const refreshToken = async (attachment) => {
-    const response = await fetch(
-      `https://iemb-backend.azurewebsites.net/api/login?username=${encodeURI(
-        getCookie("username")
-      )}&password=${encodeURI(getCookie("password"))}`
-    );
-
-    if (response.status != 200) {
-      return setInfo("Token refresh failed");
-    }
-
-    const data = await response.json();
-
-    if (data.success) {
-      setInfo(data.message);
-      setCookie("auth_token", data.AUTH_TOKEN, 1800);
-      setCookie("sess_id", data.SESSION_ID, 1800);
-      setCookie("veri_token", data.VERI_TOKEN_COOKIE, 1800);
-
-      return await downloadFile(attachment);
-    }
-
-    if (data.message === "Invalid username or password") {
-      router.push("/");
-    } else {
-      setInfo(data.message);
-    }
-  };
-
   const downloadFile = async (attachment) => {
     if (
       !getCookie("auth_token") ||
       !getCookie("sess_id") ||
       !getCookie("veri_token")
     ) {
-      return await refreshToken(attachment);
+      return await refreshToken(async () => {
+        downloadFile(attachment);
+      });
     }
 
     const response = await fetch("/api/downloadFile", {
@@ -62,7 +36,9 @@ const PostContent = ({ attachments, setInfo }) => {
       setInfo(data.message);
 
       if (data.message == "Needs to refresh token") {
-        return await refreshToken(attachment);
+        return await refreshToken(async () => {
+          downloadFile(attachment);
+        });
       } else return;
     }
 

src/pages/lost-and-found.jsx
@@ -2,10 +2,11 @@ import Head from "next/head";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 
-import { getCookie, setCookie, checkCookie } from "../lib/cookieMonster";
+import { getCookie, checkCookie } from "../lib/cookieMonster";
+import { refreshToken } from "../lib/browserMonster";
 
 import Navbar from "../components/Navbar";
-import { Snackbar, Alert, Stack } from "@mui/material";
+import { Snackbar, Alert } from "@mui/material";
 import Messages from "../components/Messages";
 import LoadingSpinner from "../components/LoadingSpinner";
 
@@ -15,35 +16,6 @@ export default function Home() {
   const [loading, setLoading] = useState(true);
   const router = useRouter();
 
-  const refreshToken = async () => {
-    const response = await fetch(
-      `https://iemb-backend.azurewebsites.net/api/login?username=${encodeURI(
-        getCookie("username")
-      )}&password=${encodeURI(getCookie("password"))}`
-    );
-
-    if (response.status != 200) {
-      return setInfo("Token refresh failed");
-    }
-
-    const data = await response.json();
-
-    if (data.success) {
-      setCookie("auth_token", data.AUTH_TOKEN, 1800);
-      setCookie("sess_id", data.SESSION_ID, 1800);
-      setCookie("veri_token", data.VERI_TOKEN_COOKIE, 1800);
-
-      setInfo("Token refreshed");
-      return await fetchMessages();
-    }
-
-    if (data.message === "Invalid username or password") {
-      router.push("/");
-    } else {
-      setInfo(data.message);
-    }
-  };
-
   const fetchMessages = async () => {
     // immediately refresh token if it has expired already
     // saves ~2s because iemb is slow...
@@ -52,7 +24,7 @@ export default function Home() {
       !checkCookie("sess_id") ||
       !checkCookie("veri_token")
     ) {
-      return await refreshToken();
+      return await refreshToken(fetchMessages);
     }
     const url = `https://iemb-backend.azurewebsites.net/api/getBoard?authToken=${encodeURI(
       getCookie("auth_token")
@@ -66,7 +38,7 @@ export default function Home() {
       setInfo(data.message);
 
       if (data.message === "Needs to refresh token") {
-        return await refreshToken();
+        return await refreshToken(fetchMessages);
       } else return;
     }
 

src/pages/service.jsx
@@ -2,10 +2,11 @@ import Head from "next/head";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 
-import { getCookie, setCookie, checkCookie } from "../lib/cookieMonster";
+import { getCookie, checkCookie } from "../lib/cookieMonster";
+import { refreshToken } from "../lib/browserMonster";
 
 import Navbar from "../components/Navbar";
-import { Snackbar, Alert, Stack } from "@mui/material";
+import { Snackbar, Alert } from "@mui/material";
 import Messages from "../components/Messages";
 import LoadingSpinner from "../components/LoadingSpinner";
 
@@ -15,35 +16,6 @@ export default function Home() {
   const [loading, setLoading] = useState(true);
   const router = useRouter();
 
-  const refreshToken = async () => {
-    const response = await fetch(
-      `https://iemb-backend.azurewebsites.net/api/login?username=${encodeURI(
-        getCookie("username")
-      )}&password=${encodeURI(getCookie("password"))}`
-    );
-
-    if (response.status != 200) {
-      return setInfo("Token refresh failed");
-    }
-
-    const data = await response.json();
-
-    if (data.success) {
-      setCookie("auth_token", data.AUTH_TOKEN, 1800);
-      setCookie("sess_id", data.SESSION_ID, 1800);
-      setCookie("veri_token", data.VERI_TOKEN_COOKIE, 1800);
-
-      setInfo("Token refreshed");
-      return await fetchMessages();
-    }
-
-    if (data.message === "Invalid username or password") {
-      router.push("/");
-    } else {
-      setInfo(data.message);
-    }
-  };
-
   const fetchMessages = async () => {
     // immediately refresh token if it has expired already
     // saves ~2s because iemb is slow...
@@ -52,7 +24,7 @@ export default function Home() {
       !checkCookie("sess_id") ||
       !checkCookie("veri_token")
     ) {
-      return await refreshToken();
+      return await refreshToken(fetchMessages);
     }
     const url = `https://iemb-backend.azurewebsites.net/api/getBoard?authToken=${encodeURI(
       getCookie("auth_token")
@@ -66,7 +38,7 @@ export default function Home() {
       setInfo(data.message);
 
       if (data.message === "Needs to refresh token") {
-        return await refreshToken();
+        return await refreshToken(fetchMessages);
       } else return;
     }
 

src/pages/student.jsx
@@ -2,10 +2,11 @@ import Head from "next/head";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 
-import { getCookie, setCookie, checkCookie } from "../lib/cookieMonster";
+import { getCookie, checkCookie } from "../lib/cookieMonster";
+import { refreshToken } from "../lib/browserMonster";
 
 import Navbar from "../components/Navbar";
-import { Snackbar, Alert, Stack } from "@mui/material";
+import { Snackbar, Alert } from "@mui/material";
 import Messages from "../components/Messages";
 import LoadingSpinner from "../components/LoadingSpinner";
 
@@ -15,35 +16,6 @@ export default function Home() {
   const [loading, setLoading] = useState(true);
   const router = useRouter();
 
-  const refreshToken = async () => {
-    const response = await fetch(
-      `https://iemb-backend.azurewebsites.net/api/login?username=${encodeURI(
-        getCookie("username")
-      )}&password=${encodeURI(getCookie("password"))}`
-    );
-
-    if (response.status != 200) {
-      return setInfo("Token refresh failed");
-    }
-
-    const data = await response.json();
-
-    if (data.success) {
-      setCookie("auth_token", data.AUTH_TOKEN, 1800);
-      setCookie("sess_id", data.SESSION_ID, 1800);
-      setCookie("veri_token", data.VERI_TOKEN_COOKIE, 1800);
-
-      setInfo("Token refreshed");
-      return await fetchMessages();
-    }
-
-    if (data.message === "Invalid username or password") {
-      router.push("/");
-    } else {
-      setInfo(data.message);
-    }
-  };
-
   const fetchMessages = async () => {
     // immediately refresh token if it has expired already
     // saves ~2s because iemb is slow...
@@ -52,7 +24,7 @@ export default function Home() {
       !checkCookie("sess_id") ||
       !checkCookie("veri_token")
     ) {
-      return await refreshToken();
+      return await refreshToken(fetchMessages);
     }
     const url = `https://iemb-backend.azurewebsites.net/api/getBoard?authToken=${encodeURI(
       getCookie("auth_token")
@@ -67,7 +39,7 @@ export default function Home() {
       setInfo(data.message);
 
       if (data.message === "Needs to refresh token") {
-        return await refreshToken();
+        return await refreshToken(fetchMessages);
       } else return;
     }
 

commit 464bc81f7215fcbef1e23bb6ac3abd38f70830f8

fix: reference errors
Hopefully resolves the reference errors introduced by commit b6b91320c555161cfb9b7fbc189d6068c7ea4694

src/components/PostContent.jsx
@@ -15,7 +15,7 @@ const PostContent = ({ attachments, setInfo }) => {
     ) {
       return await refreshToken(async () => {
         downloadFile(attachment);
-      });
+      }, setInfo);
     }
 
     const response = await fetch("/api/downloadFile", {
@@ -38,7 +38,7 @@ const PostContent = ({ attachments, setInfo }) => {
       if (data.message == "Needs to refresh token") {
         return await refreshToken(async () => {
           downloadFile(attachment);
-        });
+        }, setInfo);
       } else return;
     }
 

src/components/PostReply.jsx
@@ -32,7 +32,7 @@ const PostReply = ({ info, boardID, pid, setInfo }) => {
       !getCookie("sess_id") ||
       !getCookie("veri_token")
     ) {
-      return await refreshToken(processReply);
+      return await refreshToken(processReply, setInfo);
     }
 
     const url = `https://iemb-backend.azurewebsites.net/api/reply?authToken=${encodeURI(
@@ -57,7 +57,7 @@ const PostReply = ({ info, boardID, pid, setInfo }) => {
     setInfo(data.message);
     if (!data.success) {
       if (data.message === "Needs to refresh token") {
-        return await refreshToken(processReply);
+        return await refreshToken(processReply, setInfo);
       }
       if (data.message === "Invalid username or password") {
         router.push("/");

src/lib/browserMonster.jsx
@@ -1,4 +1,6 @@
-export const refreshToken = async (onComplete) => {
+import { getCookie, setCookie } from "./cookieMonster.jsx";
+
+export const refreshToken = async (onComplete, setInfo) => {
   const response = await fetch(
     `https://iemb-backend.azurewebsites.net/api/login?username=${encodeURI(
       getCookie("username")

src/pages/lost-and-found.jsx
@@ -24,7 +24,7 @@ export default function Home() {
       !checkCookie("sess_id") ||
       !checkCookie("veri_token")
     ) {
-      return await refreshToken(fetchMessages);
+      return await refreshToken(fetchMessages, setInfo);
     }
     const url = `https://iemb-backend.azurewebsites.net/api/getBoard?authToken=${encodeURI(
       getCookie("auth_token")
@@ -38,7 +38,7 @@ export default function Home() {
       setInfo(data.message);
 
       if (data.message === "Needs to refresh token") {
-        return await refreshToken(fetchMessages);
+        return await refreshToken(fetchMessages, setInfo);
       } else return;
     }
 

src/pages/service.jsx
@@ -24,7 +24,7 @@ export default function Home() {
       !checkCookie("sess_id") ||
       !checkCookie("veri_token")
     ) {
-      return await refreshToken(fetchMessages);
+      return await refreshToken(fetchMessages, setInfo);
     }
     const url = `https://iemb-backend.azurewebsites.net/api/getBoard?authToken=${encodeURI(
       getCookie("auth_token")
@@ -38,7 +38,7 @@ export default function Home() {
       setInfo(data.message);
 
       if (data.message === "Needs to refresh token") {
-        return await refreshToken(fetchMessages);
+        return await refreshToken(fetchMessages, setInfo);
       } else return;
     }
 

src/pages/student.jsx
@@ -24,7 +24,7 @@ export default function Home() {
       !checkCookie("sess_id") ||
       !checkCookie("veri_token")
     ) {
-      return await refreshToken(fetchMessages);
+      return await refreshToken(fetchMessages, setInfo);
     }
     const url = `https://iemb-backend.azurewebsites.net/api/getBoard?authToken=${encodeURI(
       getCookie("auth_token")
@@ -39,7 +39,7 @@ export default function Home() {
       setInfo(data.message);
 
       if (data.message === "Needs to refresh token") {
-        return await refreshToken(fetchMessages);
+        return await refreshToken(fetchMessages, setInfo);
       } else return;
     }
 