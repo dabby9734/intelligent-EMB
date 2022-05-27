150cb972b91fa12b8267b247128cda16b651eb92
Liu Weichu <weichufmsp@gmail.com>
2022-05-27 10:56:44 +0800

    fix: QOL improvements
    - responsive feedback on login page when refreshing session
    - login page "remember me" checkbox is no longer checked by default
    - removed an unused import for login page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,21 +1,41 @@
 import Head from "next/head";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
+import { Alert, Snackbar } from "@mui/material";
 
 import { getCookie, setCookie } from "../lib/cookieMonster";
+import { refreshToken } from "../lib/browserMonster";
 
 import Login from "../components/Login";
 
 export default function LoginPage() {
   const router = useRouter();
+  const [info, setInfo] = useState("");
 
   useEffect(() => {
     if (getCookie("username") && getCookie("password")) {
       // update cookie
       setCookie("username", getCookie("username"), 2592000);
 
-      // redirect to student page
-      router.push("/student?type=inbox");
+      if (
+        !getCookie("sess_id") ||
+        !getCookie("veri_token") ||
+        !getCookie("auth_token")
+      ) {
+        refreshToken(
+          // redirect to student board if token refresh successful
+          async () => {
+            setTimeout(() => {
+              setInfo("Redirecting...");
+            }, 500);
+            setTimeout(() => {
+              router.push("/student?type=inbox");
+            }, 1000);
+          },
+          setInfo,
+          router
+        );
+      }
     }
   });
 
@@ -27,6 +47,14 @@ export default function LoginPage() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      <Snackbar
+        open={!!info}
+        autoHideDuration={5000}
+        anchorOrigin={{ vertical: "top", horizontal: "center" }}
+        onClose={() => setInfo("")}
+      >
+        <Alert severity="info">{info}</Alert>
+      </Snackbar>
       <Login />
     </div>
   );
