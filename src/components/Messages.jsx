cf6dcd5cccdea768b58b21b55299349f6675232f
Liu Weichu <weichufmsp@gmail.com>
2023-03-23 22:09:56 +0800

    refactor: clean up `fetchMessages()` function

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -11,6 +11,57 @@ import { getApiURL } from "../lib/util";
 import PostFrame from "./PostFrame";
 import CloseIcon from "@mui/icons-material/Close";
 
+const fetchMessages = async (type, boardID, page = 1) => {
+  if (
+    !checkCookie("auth_token") ||
+    !checkCookie("sess_id") ||
+    !checkCookie("veri_token")
+  ) {
+    return -2;
+  }
+
+  let endpoint = "getBoard";
+  let url = new URL(getApiURL(endpoint));
+  switch (type) {
+    case "updated-messages":
+      url.searchParams.append("isupdated", "True");
+      url.searchParams.append("t", "1");
+      break;
+    case "my-messages":
+      url.searchParams.append("postBy", localStorage.getItem("name"));
+      url.searchParams.append("t", "2");
+      break;
+    case "my-drafts":
+      url.searchParams.append("t", "3");
+      break;
+    case "starred":
+      endpoint = "getBoardStarred";
+      url = new URL(getApiURL("getBoardStarred"));
+      break;
+    case "archived":
+      url = new URL(getApiURL("getBoardArchived"));
+      url.searchParams.append("page", page);
+      break;
+  }
+  url.searchParams.append("boardID", boardID);
+  const response = await fetch(url).catch((err) => {
+    return -1;
+  });
+
+  switch (response.status) {
+    case 401:
+      return -2;
+    case 200:
+      break;
+    default:
+      // also handles response code 500
+      return -1;
+  }
+
+  const data = await response.json();
+  return data;
+};
+
 const Messages = ({ boardID }) => {
   // essentials
   const router = useRouter();
@@ -26,82 +77,6 @@ const Messages = ({ boardID }) => {
   const [fav, setFav] = useState("");
   const { type } = router.query;
 
-  const fetchMessages = async (type, setM = true) => {
-    // immediately refresh token if it has expired already
-    // saves ~2s because iemb is slow...
-    if (
-      !checkCookie("auth_token") ||
-      !checkCookie("sess_id") ||
-      !checkCookie("veri_token")
-    ) {
-      return await refreshToken(
-        async () => fetchMessages(type, setM),
-        notif.open
-      );
-    }
-
-    let endpoint = "getBoard";
-    let url = new URL(getApiURL(endpoint));
-    switch (type) {
-      case "updated-messages":
-        url.searchParams.append("isupdated", "True");
-        url.searchParams.append("t", "1");
-        break;
-      case "my-messages":
-        url.searchParams.append("postBy", localStorage.getItem("name"));
-        url.searchParams.append("t", "2");
-        break;
-      case "my-drafts":
-        url.searchParams.append("t", "3");
-        break;
-      case "starred":
-        endpoint = "getBoardStarred";
-        url = new URL(getApiURL("getBoardStarred"));
-        break;
-      case "archived":
-        url = new URL(getApiURL("getBoardArchived"));
-        url.searchParams.append("page", page);
-        break;
-    }
-    url.searchParams.append("boardID", boardID);
-    const response = await fetch(url).catch((err) => {
-      return notif.open("An error occured while fetching messages");
-    });
-
-    switch (response.status) {
-      case 401:
-        return await refreshToken(
-          async () => fetchMessages(type, setM),
-          notif.open
-        );
-      case 200:
-        break;
-      default:
-        // also handles response code 500
-        return notif.open("An error occured while fetching messages");
-    }
-
-    const data = await response.json();
-
-    // add messages to localStorage
-    localStorage.setItem(`${boardID}+${type}`, JSON.stringify(data.messages));
-    // add user's name to localStorage
-    if (data.name) {
-      localStorage.setItem("name", data.name);
-    }
-    // pagination data for archived and starred boards
-    if ((type === "archived" || type === "starred") && setM) {
-      setPage(data.currentPage);
-      setTotalPages(data.totalPages);
-    }
-    if (setM) {
-      setMessages(data.messages);
-      notif.open("Messages fetched");
-    }
-    if (type === "starred") {
-      setFav(data.messages);
-    }
-  };
   const getSortedMessages = (messages) => {
     messages?.sort((a, b) => (a.pid < b.pid ? 1 : -1));
     // sort by pid
@@ -136,27 +111,45 @@ const Messages = ({ boardID }) => {
   // initiates fetching messages
   useEffect(() => {
     async function fetchData() {
-      if (type) {
-        try {
-          setMessages(JSON.parse(localStorage.getItem(`${boardID}+${type}`)));
-        } catch (err) {
-          console.log(err);
-        }
-        try {
-          setFav(JSON.parse(localStorage.getItem(`${boardID}+starred`)));
-        } catch (err) {
-          console.log(err);
-        }
+      if (!type) return;
+      // update UI with cached messages
+      try {
+        setMessages(JSON.parse(localStorage.getItem(`${boardID}+${type}`)));
+      } catch (err) {
+        console.log(err);
+      }
+      try {
+        setFav(JSON.parse(localStorage.getItem(`${boardID}+starred`)));
+      } catch (err) {
+        console.log(err);
+      }
 
-        if (type !== "starred") {
-          await Promise.all([
-            fetchMessages(type),
-            fetchMessages("starred", false),
-          ]);
-        } else {
-          // for starred board
-          await fetchMessages(type);
-        }
+      // fetch fresh messages
+      const data = await fetchMessages(type, boardID, page);
+      // handle error
+      if (data === -1) return notif.open("Error fetching messages");
+      if (data === -2)
+        return await refreshToken(
+          async () => fetchMessages(type, boardID, page),
+          notif.open
+        );
+      // add messages to localStorage
+      localStorage.setItem(`${boardID}+${type}`, JSON.stringify(data.messages));
+      // add user's name to localStorage
+      if (data.name) localStorage.setItem("name", data.name);
+      // pagination data for archived and starred boards
+      if (type === "archived" || type === "starred") {
+        setPage(data.currentPage);
+        setTotalPages(data.totalPages);
+      }
+      // update UI with fresh messages
+      setMessages(data.messages);
+      notif.open("Messages fetched");
+      // fetch starred messages (to update star icon)
+      if (type !== "starred") {
+        fetchMessages("starred", boardID).then((data) => {
+          setFav(data.messages);
+        });
       }
     }
     fetchData();
@@ -165,10 +158,27 @@ const Messages = ({ boardID }) => {
   // fetches messages again if the user navigates to a different page
   // for archived and starred board only
   useEffect(() => {
-    if (router.query.type === "archived" || router.query.type === "starred") {
-      setMessages("");
-      fetchMessages(router.query.type, page);
+    async function fetchData() {
+      if (router.query.type === "archived" || router.query.type === "starred") {
+        setMessages("");
+        // fetch fresh messages
+        const data = fetchMessages(type, boardID, page);
+        // handle error
+        if (data === -1) return notif.open("Error fetching messages");
+        if (data === -2)
+          return await refreshToken(
+            async () => fetchMessages(type),
+            boardID,
+            page
+          );
+        // update UI with fresh messages
+        setMessages(data.messages);
+        setPage(data.currentPage);
+        setTotalPages(data.totalPages);
+        notif.open("Messages fetched");
+      }
     }
+    fetchData();
   }, [page]);
 
   // toggles loading state
